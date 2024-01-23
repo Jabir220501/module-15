b229479b0905b58fefdab86a16a3d56763f62fd1
Jabir <220501@student.glu.nl>
2024-01-23 15:53:12 +0100

    Rows are added and its working with the API

--- src/components/Row/Row.js
+++ src/components/Row/Row.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from "../../service/api.js";
+import './Row.css'
 
-function Row() {
+function Row({ title, fetchUrl, isLargeRow = false }) {
+    const [movies, setMovies] = useState ([])
+
+    const base_url = "https://image.tmdb.org/t/p/original/"
+    
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(fetchUrl);
+            setMovies(request.data.results);
+            return request;
+        }
+        fetchData()
+    }, [fetchUrl])
+
+
+    console.log(movies)
+
   return (
-    <div>Row</div>
+    <div className='row'>
+        <h2>{title}</h2>
+
+            <div className="row__posters">
+            {movies.map(
+                (movie) => 
+                ((isLargeRow && movie.poster_path) ||
+                (!isLargeRow && movie.backdrop_path)) && (
+                    <img 
+                    className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                    key={movies.id}
+                    src={`${base_url}${
+                        isLargeRow ? movie.poster_path : movie.backdrop_path
+                    }`} 
+                    alt="{movie.name}" 
+                    />
+                )
+            )}
+            </div>
+    </div>
   )
 }
 
