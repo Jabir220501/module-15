2b131b92015fe21d1d99c7833cb1b63973624e55
Jabir <220501@student.glu.nl>
2024-02-01 14:59:37 +0100

    Missing movie works, changed request of the TMDB API

--- src/utils/useFetch.js
+++ src/utils/useFetch.js
@@ -3,8 +3,8 @@ const API_KEY = "a527eb9bc54324ce4f762ac2b6d36ae6";
 const request = {
   fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
-  fetchTopRated: `/top_rated?api_key=${API_KEY}&language=en-US&page=1`,
-  fetchActionMovies: `/keyword/{keyword_id}/movies?api_key=${API_KEY}&language=en-US`,
+  fetchTopRated: `/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=en-US&page=1&sort_by=vote_average.desc&without_genres=99,10755&vote_count.gte=20`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=Action`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
   fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
   fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
